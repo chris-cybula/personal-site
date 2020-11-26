f382d9c44f32a53c14c37f87a29b23ff9c11dc0c
chris-cybula <chris.cybula@gmail.com>
2020-11-26 12:17:25 +0000

    Install styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Chris</h1>
+    <Container>
+      <h1>Chris</h1>
+    </Container>
   </Layout>
 )
 
