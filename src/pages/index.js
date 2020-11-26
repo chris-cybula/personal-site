40d98b73ead4b18da470b7a74abfaa3856af38ba
chris-cybula <chris.cybula@gmail.com>
2020-11-26 20:48:21 +0000

    Add basic animation on background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,46 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import styled from "styled-components"
-import img from '../images/bg.jpg';
+import styled, { keyframes } from "styled-components"
+import img from "../images/bg.jpg"
 
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
   background-image: url(${img});
-	  background-position: center;
-	  background-size: cover;
-	  background-repeat: no-repeat;
-	  z-index: 1;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  z-index: 1;
+  
+  &::before {
+    content: '';
+    display: block;
+	  position: absolute;
+	  top: 0;
+	  left: 0;
+	  width: 100%;
+	  height: 100%;
+    background-color: black;
+    animation: fadeInAnimation ease 3s; 
+    animation-iteration-count: 1; 
+    animation-fill-mode: forwards; 
+  }
+
+  @keyframes fadeInAnimation { 
+    0% { 
+        opacity: 1; 
+    } 
+    100% { 
+        opacity: 0; 
+     } 
+} 
 `
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Container>
-      
-    </Container>
+    <Container></Container>
   </Layout>
 )
 
