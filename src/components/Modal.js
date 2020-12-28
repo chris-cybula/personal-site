439a71f57eadc281d1796790ca80c90e2741ad5c
Chris <chris.cybula@gmail.com>
2020-12-28 16:06:51 +0000

    Add modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -72,13 +72,14 @@ const MockupContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: relative;
 
   @media (min-width: 768px) {
     width: 450px;
   }
 
   @media (min-width: 576px) { 
-    height: 200px;
+    height: 180px;
   }
 `
 
@@ -122,6 +123,34 @@ const Image = styled.img`
   padding-left: 8px;
 `
 
+const Text = styled.p`
+  margin-left: 20px;
+  padding-bottom: 25px;
+`
+
+const Button = styled.button`
+  position: absolute;
+  bottom: 25px;
+  right: 25px;
+  background: linear-gradient(#81c5fd, #3389ff);
+  color: white;
+  border: none;
+  height: 22px;
+  width: 70px;
+  cursor: pointer;
+  border-radius: 5px;
+  pointer-events: initial;
+
+  &:active {
+    background: linear-gradient(#4faefc, #006bff);
+  }
+`
+
+const Icon = styled.svg`
+  fill: #555555;
+  margin-bottom: 25px;
+`
+
 const Browser = () => {
   const refContainer = React.useRef()
   const refMockupWrapper = React.useRef()
@@ -159,10 +188,8 @@ const Browser = () => {
     refMockupWrapper.current.style.pointerEvents = "initial"
   }
 
-  const closeBrowser = e => {
+  const closeModal = e => {
     dispatch(setOpenModal(false));
-
-    console.log('Chris')
   }
 
   return (
@@ -177,14 +204,28 @@ const Browser = () => {
 
           <MockupBar>
             <MockupButtons>
-              <MockupButton modifiers={"close"} onClick={ closeBrowser }></MockupButton>
+              <MockupButton modifiers={"close"} onClick={closeModal}></MockupButton>
               <MockupButton></MockupButton>
               <MockupButton></MockupButton>
             </MockupButtons>
           </MockupBar>
 
           <MockupContent>
-            <p>Site has been successfully deleted.</p>
+            <Icon
+              id="Layer_1"
+              enableBackground="new 0 0 512 512"
+              height="60"
+              viewBox="0 0 512 512"
+              width="60"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path d="m424 64h-88v-16c0-26.51-21.49-48-48-48h-64c-26.51 0-48 21.49-48 48v16h-88c-22.091 0-40 17.909-40 40v32c0 8.837 7.163 16 16 16h384c8.837 0 16-7.163 16-16v-32c0-22.091-17.909-40-40-40zm-216-16c0-8.82 7.18-16 16-16h64c8.82 0 16 7.18 16 16v16h-96z" />
+                <path d="m78.364 184c-2.855 0-5.13 2.386-4.994 5.238l13.2 277.042c1.22 25.64 22.28 45.72 47.94 45.72h242.98c25.66 0 46.72-20.08 47.94-45.72l13.2-277.042c.136-2.852-2.139-5.238-4.994-5.238zm241.636 40c0-8.84 7.16-16 16-16s16 7.16 16 16v208c0 8.84-7.16 16-16 16s-16-7.16-16-16zm-80 0c0-8.84 7.16-16 16-16s16 7.16 16 16v208c0 8.84-7.16 16-16 16s-16-7.16-16-16zm-80 0c0-8.84 7.16-16 16-16s16 7.16 16 16v208c0 8.84-7.16 16-16 16s-16-7.16-16-16z" />
+              </g>
+            </Icon>
+            <Button onClick={closeModal}>OK</Button>
+            <Text>Site has been successfully deleted.</Text>
           </MockupContent>
         </MockupWrapper>
       </Container>
