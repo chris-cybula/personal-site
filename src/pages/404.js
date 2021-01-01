f268ffbfcd86d849d6c42b068cc27d32ddb16671
Chris <chris.cybula@gmail.com>
2021-01-01 18:00:43 +0000

    Style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/Seo"
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  color: white;
+`
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>404</h1>
+    </Container>
   </Layout>
 )
 
