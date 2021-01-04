79ef90201315c9c2f40215fcea5a0d0ecde94252
Chris <chris.cybula@gmail.com>
2021-01-04 21:43:26 +0000

    Fix redux issue

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -5,5 +5,5 @@ export let store = createStore(
   allReducers,
 
   // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
